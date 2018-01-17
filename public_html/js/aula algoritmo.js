3e055cf003b3c1341683ba7acaf15a47d1ed987e
a9237348 <a9237348@02LAB000PC01.rn.senac.br>
2018-01-17 19:54:52 -0300

    Aula de Botstrap

---
+++ public_html/js/aula algoritmo.js
@@ -0,0 +1,18 @@
+function eleicao() {
+    escolha = parseInt(prompt("Escolha o seu candidado de 1 a 4, nulo outro valor"));
+    switch(escolha)
+        case 1:
+            alert("Candidato 1");
+            break;
+        case 2:
+            alert("Candidato 2");
+            break;
+        case 3:
+            alert("Candidato 3");
+            break;
+        case 4:
+            alert("Candidato 4");
+            break;
+        default:
+            alert("Nulo");
+}
