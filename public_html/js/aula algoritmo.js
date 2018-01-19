9efa2e95c0f7e3b4fc5d60ffc10c1578a51be4d5
a9237348 <a9237348@02LAB000PC01.rn.senac.br>
2018-01-18 21:22:28 -0300

    Aula de Botstrap

--- public_html/js/aula algoritmo.js
+++ public_html/js/aula algoritmo.js
@@ -18,6 +18,87 @@
 //    }
 //}
 //
+//        Exercicio 1
+//            var alo = alert("Alo Mundo");
+//       
+//       
+//        Exercicio 2
+//            Digitando o Numero
+//            var n1 = prompt("Informe o Numero")
+//            
+//            Informando o Numero
+//            var n= alert("Seu Numero é " + n1)
+//            
+//
+//        
+//        Exercicio 3 Somando Valores
+//            Digitando o Numero
+//            var n1 = parseFloat(prompt("Informe o Numero 1"));
+//            var n2 = parseFloat(prompt("Informe o Numero 2"));
+//            var valor = (n1 + n2);
+//            Informando o Numero
+//            alert("Seu Numero é " + valor );
+//            
+//
+//        
+// Exercicio 4 Média ponderada
+//            informando as notas
+//            var n1 = parseFloat(prompt("Infome a nota 1"));
+//            var n2 = parseFloat(prompt("Infome a nota 2"));
+//            var n3 = parseFloat(prompt("Infome a nota 3"));
+//            var n4 = parseFloat(prompt("Infome a nota 4"));
+//            
+//            //calculando a média
+//            var m = (n1*1 + n2*2 + n3*3 + n4*4) / 10;
+//            
+//            //Exibindo a média
+//            alert("Sua Média é " + m.toFixed(2));
+//
+//        
+// Exercicio 5 Convertendo metros em centimetros
+//            var m = parseFloat(prompt("Metros"));
+//            con = m*100;
+//            alert("Centimetros" + con);
+//            
+//
+//       
+// Exercicio 6 calculando raio
+//           pi = 3.14
+//           r = parseFloat(prompt("Informe a área"));
+//           cal = pi * (r * r5);
+//           alert("Sua área é "+cal + "m²");
+//
+//       
+// Exercicio 7 Area do quadrado ao ²
+//           altura = parseFloat(prompt("Informe a Altura"));
+//           largura = parseFloat(prompt("Informe a Largura"));
+//           area = altura * largura;
+//           alert("Sua Área " + area*2);
+//
+//       
+// Exercicio 8 
+//           d = parseFloat(prompt("Informe quanto você ganha por hora"));
+//           h = parseFloat(prompt("Informe a quantidade de horas"));
+//           r = d * h;
+//           alert("Seu salario é: " + r);
+//
+// Exercicio 9
+//          //informe graus
+//          
+//          var f = parseFloat(prompt("Informe o valor Farenheit"));
+//          vf = (f - 32);
+//          c = (5 * vf / 9);
+//          alert("Seu valor em Cº é: " + c.toFixed(2));
+//          
+//
+//        
+// Exercicio 9
+//            var c = parseFloat(prompt("Digite o valor de Cº"))
+//            
+//            var v = c * 1.8 
+//            var f = v + 32
+//            
+//            alert("O valor é: " + f.toFixed(2)) -->
 //function onze() {
 //    //apresentando os numeros
 //    n1 = parseFloat(prompt("Digite o 1º numero inteiro"));
@@ -61,18 +142,18 @@
 //}
 //
 //    
-////    switch (sexo){
-////        case 1:
-////            altura = parseFloat(prompt("Sua altura é:"));
-////            Am = (72.7 * altura) - 58;
-////            alert("Seu peso ideal masculino é:" + Am);
-////            break;
-////        case 2 :
-////            alturaf = parseFloat(prompt("Sua altura é:"));
-////            Af = (62.1 * alturaf) - 44.7;
-////            alert("Seu peso ideal feminino é:" + Af);
-////            break;
-////    }
+//    switch (sexo){
+//        case 1:
+//            altura = parseFloat(prompt("Sua altura é:"));
+//            Am = (72.7 * altura) - 58;
+//            alert("Seu peso ideal masculino é:" + Am);
+//            break;
+//        case 2 :
+//            alturaf = parseFloat(prompt("Sua altura é:"));
+//            Af = (62.1 * alturaf) - 44.7;
+//            alert("Seu peso ideal feminino é:" + Af);
+//            break;
+//    }
 //}
 //
 //function quatorze() {
---
+++ public_html/js/exercicio repeticao.js
@@ -0,0 +1,15 @@
+//function um() {
+//    for (var i=0; i<100; i++){
+//         n = parseFloat(prompt("Digite o numero de 0 a 10"))
+//         if(n <= 10 >0 ){
+//             break
+//         }
+//    }
+//}
+//
+
+function dois() {
+    for (var i=0; i<100; i++){
+        
+    }
+}
